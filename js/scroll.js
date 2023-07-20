0d9e3d6e663931c684bf7ea42d9847597714cb66
Ralphie Celedon <ralphie94@pacbell.net>
2023-07-20 12:42:38 -0700

    created footer

--- js/scroll.js
+++ js/scroll.js
@@ -1,32 +1,5 @@
-// document.lastScrollPosition = 0;
-// document.lastCentered = 0;
-// document.onWayTo = null;
-
-// document.addEventListener('scroll', () => {
-//   const direction = window.pageYOffset - document.lastScrollPosition > 0 ? 'down' : 'up';
-//   const sections = [...document.querySelectorAll('section')];
-
-//   if (document.onWayTo === null) {
-//     const destIndex = direction === 'up' ? document.lastCentered - 1 : document.lastCentered + 1;
-//     if (destIndex >= 0 && destIndex < sections.length) {
-//       console.log({destIndex,direction});
-//       document.onWayTo = destIndex;
-//       window.scroll(0, sections[destIndex].offsetTop);
-//     }
-//   }
-
-
-//   sections.forEach((section,index) => {
-//     if (window.pageYOffset === section.offsetTop) {
-//       document.lastCentered = index;
-//       section.className = 'active';
-//       if (document.onWayTo === index) {
-//         document.onWayTo = null;
-//       }
-//     } else {
-//       section.className = '';
-//     }
-//   })
-
-//   document.lastScrollPosition = window.pageYOffset;
-// })
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $(".content").css("opacity", 1 - $(window).scrollTop() / 250)
+    })
+});
